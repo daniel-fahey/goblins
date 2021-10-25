2969393c72626fe16a65feb10d133fe1ebfcf160
Christine Lemmer-Webber <cwebber@dustycloud.org>
2021-10-25 15:58:09 -0400

    start of test-proto-vats, with spawn-delivery-agent

---
+++ pre-goblins/test-proto-vats.scm
@@ -0,0 +1,27 @@
+(define-module (pre-goblins test-proto-vats)
+  #:use-module (fibers)
+  #:use-module (fibers channels)
+  #:use-module (fibers conditions)
+  #:use-module (fibers operations)
+  #:use-module (ice-9 match)
+  #:use-module (ice-9 q)
+  #:use-module (srfi srfi-64)
+  #:use-module (pre-goblins proto-vats)
+  )
+
+(test-begin "test-proto-vats")
+
+;; Testing that the delivery agent queues and de-queues messages
+(run-fibers
+ (lambda ()
+   (define-values (enq-ch deq-ch stop?)
+     (spawn-delivery-agent))
+   (put-message enq-ch 'foo)
+   (put-message enq-ch 'bar)
+   (put-message enq-ch 'baz)
+   (test-equal 'foo (get-message deq-ch))
+   (test-equal 'bar (get-message deq-ch))
+   (test-equal 'baz (get-message deq-ch)))
+ #:drain? #t)
+
+(test-end "test-proto-vats")
