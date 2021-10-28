cfeea5fc10403bfacc7ed01fcc2ddc515ae1360a
Christine Lemmer-Webber <cwebber@dustycloud.org>
2021-10-28 16:50:55 -0400

    copyright stuff

--- goblins/actor-lib/methods.scm
+++ goblins/actor-lib/methods.scm
@@ -1,3 +1,17 @@
+;;; Copyright 2019-2021 Christine Lemmer-Webber
+;;;
+;;; Licensed under the Apache License, Version 2.0 (the "License");
+;;; you may not use this file except in compliance with the License.
+;;; You may obtain a copy of the License at
+;;;
+;;;    http://www.apache.org/licenses/LICENSE-2.0
+;;;
+;;; Unless required by applicable law or agreed to in writing, software
+;;; distributed under the License is distributed on an "AS IS" BASIS,
+;;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;; See the License for the specific language governing permissions and
+;;; limitations under the License.
+
 (define-module (goblins actor-lib methods)
   #:use-module (ice-9 match)
   #:use-module (srfi srfi-1)
