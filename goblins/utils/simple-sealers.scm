ba20af4753699b3c52603625c924d32adc547a23
Christine Lemmer-Webber <cwebber@dustycloud.org>
2021-10-29 21:07:40 -0400

    Oops, fix simple-sealers module name

--- goblins/utils/simple-sealers.scm
+++ goblins/utils/simple-sealers.scm
@@ -12,7 +12,7 @@
 ;;; See the License for the specific language governing permissions and
 ;;; limitations under the License.
 
-(define-module (goblins simple-sealers)
+(define-module (goblins utils simple-sealers)
   #:export (make-sealer-triplet)
   #:use-module (srfi srfi-9)
   #:use-module (srfi srfi-9 gnu))
