3cdc516f1bbe6fb8796acc6698659f6c395c681a
Christine Lemmer-Webber <cwebber@dustycloud.org>
2021-10-29 22:19:11 -0400

    weak-box.scm

---
+++ goblins/utils/weak-box.scm
@@ -0,0 +1,31 @@
+;;; Copyright 2021 Christine Lemmer-Webber
+;;;
+;;; Licensed under the Apache License, Version 2.0 (the "License");
+;;; you may not use this file except in compliance with the License.
+;;; You may obtain a copy of the License at
+;;;
+;;;    http://www.apache.org/licenses/LICENSE-2.0
+;;;
+;;; Unless required by applicable law or agreed to in writing, software
+;;; distributed under the License is distributed on an "AS IS" BASIS,
+;;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;; See the License for the specific language governing permissions and
+;;; limitations under the License.
+
+(define-module (goblins utils weak-box)
+  #:use-module (ice-9 weak-vector)
+  #:use-module (srfi srfi-9)
+  #:export (make-weak-box
+            weak-box-ref
+            weak-box?))
+
+(define-record-type <weak-box>
+  (_make-weak-box wvec)
+  weak-box?
+  (wvec weak-box-wvec))
+
+(define (make-weak-box val)
+  (make-weak-vector 1 val))
+
+(define (weak-box-ref weak-box)
+  (weak-vector-ref (weak-box-wvec weak-box) 0))
