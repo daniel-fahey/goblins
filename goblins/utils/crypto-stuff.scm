64bdfeaac2f0b0c08af5b9386b4ab6ffea314ced
Christine Lemmer-Webber <cwebber@dustycloud.org>
2021-10-30 22:02:58 -0400

    url-base64-encode

--- goblins/utils/crypto-stuff.scm
+++ goblins/utils/crypto-stuff.scm
@@ -5,11 +5,17 @@
   #:use-module (gcrypt hash)
   #:use-module (gcrypt pk-crypto)
   #:use-module (gcrypt random)
+  #:use-module (gcrypt base64)
+  #:use-module (rnrs bytevectors)
   #:export (sha256d
-            strong-random-bytes))
+            strong-random-bytes
+            url-base64-encode))
 
 (define (sha256d input)
   (sha256 (sha256 input)))
 
 (define (strong-random-bytes byte-size)
   (gen-random-bv 32 %gcry-strong-random))
+
+(define (url-base64-encode bv)
+  (base64-encode bv 0 (bytevector-length bv) #f #t base64url-alphabet))
