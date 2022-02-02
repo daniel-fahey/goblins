51372e61dc3fa4410e029d665ef252a1f42df0ef
Christine Lemmer-Webber <cwebber@dustycloud.org>
2022-02-02 14:14:14 -0500

    Re-ran "hall scan", pulling in more files

--- hall.scm
+++ hall.scm
@@ -15,21 +15,57 @@ of the library!")
   (dependencies `())
   (files (libraries
            ((scheme-file "goblins")
-            (directory "goblins" ())))
+            (directory
+              "goblins"
+              ((directory
+                 "ocapn"
+                 ((scheme-file "crypto-stubs")
+                  (scheme-file "define-recordable")
+                  (scheme-file "structs-urls")
+                  (scheme-file "captp")))
+               (directory
+                 "utils"
+                 ((scheme-file "simple-sealers")
+                  (scheme-file "bytes-stuff")
+                  (scheme-file "crypto-stuff")
+                  (scheme-file "weak-box")
+                  (scheme-file "assert-type")
+                  (scheme-file "simple-dispatcher")))
+               (directory
+                 "actor-lib"
+                 ((scheme-file "methods")
+                  (scheme-file "nonce-registry")
+                  (scheme-file "cell")
+                  (scheme-file "common")
+                  (scheme-file "ward")
+                  (scheme-file "swappable")))
+               (scheme-file "ghash")
+               (scheme-file "vat")
+               (scheme-file "vrun")
+               (scheme-file "core")
+               (scheme-file "inbox")))))
          (tests ((directory
                    "tests"
-                   ((scheme-file "test-inbox")
+                   ((directory
+                      "utils"
+                      ((scheme-file "test-bytes-stuff")))
+                    (directory
+                      "actor-lib"
+                      ((scheme-file "test-ward")
+                       (scheme-file "test-common")
+                       (scheme-file "test-swappable")
+                       (scheme-file "test-cell")))
+                    (scheme-file "test-inbox")
                     (scheme-file "test-ghash")
-                    (scheme-file "test-core")
-                    (scheme-file "test-await")))))
+                    (scheme-file "test-await")
+                    (scheme-file "test-core")))))
          (programs ((directory "scripts" ())))
          (documentation
            ((org-file "README")
             (symlink "README" "README.org")
             (text-file "HACKING")
-            (text-file "COPYING")
-            (directory "doc" ((texi-file "goblins")))))
+            (symlink "COPYING" "LICENSE.txt")
+            (directory "doc" ((texi-file "goblins")))
+            (text-file "AUTHORS")))
          (infrastructure
-           ((scheme-file "guix")
-            (text-file ".gitignore")
-            (scheme-file "hall")))))
+           ((scheme-file "guix") (scheme-file "hall")))))
