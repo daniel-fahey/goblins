fc806ca7fe28819f745f998ad56b1bdf89420a98
Christine Lemmer-Webber <cwebber@dustycloud.org>
2022-02-02 14:11:53 -0500

    Add tests to hall.scm

--- hall.scm
+++ hall.scm
@@ -4,8 +4,10 @@
   (version "0.1-pre")
   (author "Christine Lemmer-Webber")
   (copyright (2022))
-  (synopsis "A transactional, distributed object programming environment")
-  (description "Spritely Goblins is a transactional, distributed object programming
+  (synopsis
+    "A transactional, distributed object programming environment")
+  (description
+    "Spritely Goblins is a transactional, distributed object programming
 environment following object capability principles.  This is the guile version
 of the library!")
   (home-page "https://spritelyproject.org/")
@@ -14,7 +16,12 @@ of the library!")
   (files (libraries
            ((scheme-file "goblins")
             (directory "goblins" ())))
-         (tests ((directory "tests" ())))
+         (tests ((directory
+                   "tests"
+                   ((scheme-file "test-inbox")
+                    (scheme-file "test-ghash")
+                    (scheme-file "test-core")
+                    (scheme-file "test-await")))))
          (programs ((directory "scripts" ())))
          (documentation
            ((org-file "README")
