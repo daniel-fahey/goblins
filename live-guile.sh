78b5f3152ddf87af1b40abaec5e5961013902b52
Christine Lemmer-Webber <cwebber@dustycloud.org>
2022-02-05 19:15:01 -0500

    live-guile.sh

---
+++ live-guile.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Remove stale file if it exists
+rm -f /tmp/guile-goblins.sock
+
+echo "***********************************************"
+echo "** Setting up Guile to listen on a socket... **"
+echo "**                                           **"
+echo "** Connect in emacs via:                     **"
+echo "**   M-x geiser-connect-local <RET>          **"
+echo "**       guile <RET>                         **"
+echo "**       /tmp/guile-goblins.sock <RET>       **"
+echo "***********************************************"
+echo ""
+
+guix environment -l guix.scm -- ./pre-inst-env guile --listen=/tmp/guile-goblins.sock
+
+# clean up, be nice
+rm /tmp/guile-goblins.sock
