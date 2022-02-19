59a3bd3efe3251213b18991d288ec9f4c0dd838f
Christine Lemmer-Webber <cwebber@dustycloud.org>
2022-02-19 17:33:07 -0500

    bcom which is unused, so call it _bcom

--- goblins/core.scm
+++ goblins/core.scm
@@ -2158,7 +2158,7 @@
                              #:key [catch-errors? #t]
                              [waiters #f])
   (define-values (actor-refr new-actormap)
-    (actormap-spawn (make-transactormap actormap) (lambda (bcom) thunk)))
+    (actormap-spawn (make-transactormap actormap) (lambda (_bcom) thunk)))
   (define-values (returned-val new-actormap2 new-msgs)
     (actormap-churn new-actormap (make-message actor-refr #f '())
                     #:catch-errors? catch-errors?
