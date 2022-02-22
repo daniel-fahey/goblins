072e7353fe9236c43d501b1496cb398be9ecdf43
Christine Lemmer-Webber <cwebber@dustycloud.org>
2022-02-22 15:06:51 -0500

    Add copyright waiver lines to a couple of files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+## This file is waived into the public domain, as-is, no warranty provided.
+##
+## If the public domain doesn't exist where you live, consider
+## this a license which waives all copyright and neighboring intellectual
+## restrictions laws mechanisms, to the fullest extent possible by law,
+## as-is, no warranty provided.
+##
+## No attribution is required and you are free to copy-paste and munge
+## into your own project.
+
 if [ ! -e "configure.ac" ]; then
     hall dist -x
 fi
--- manifest.scm
+++ manifest.scm
@@ -1,3 +1,13 @@
+;; This file is waived into the public domain, as-is, no warranty provided.
+;;
+;; If the public domain doesn't exist where you live, consider
+;; this a license which waives all copyright and neighboring intellectual
+;; restrictions laws mechanisms, to the fullest extent possible by law,
+;; as-is, no warranty provided.
+;;
+;; No attribution is required and you are free to copy-paste and munge
+;; into your own project.
+
 (use-modules (guix packages)
              (gnu packages guile-xyz)
              (gnu packages emacs)
