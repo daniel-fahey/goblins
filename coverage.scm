bf787db0fe6b5824bcd928c73232c67e7b381370
Jessica Tallon <tsyesika@tsyesika.se>
2022-05-02 13:38:23 +0200

    Backports coverage-data->lcov from bug54911
    This backports the modules keyword from the patch on upstream guile
    bug 54911. This allows us to both limit the displayed statistics to
    just goblins code, as well as ensure not to include other modules.

--- coverage.scm
+++ coverage.scm
@@ -14,9 +14,52 @@
 
 (use-modules (system vm coverage)
              (system vm vm)
+	     (srfi srfi-11)
 	     (srfi srfi-64)
 	     (ice-9 ftw)
 	     (ice-9 match))
+
+;; Backported from guile (modules/system/vm/coverage.scm), this code
+;; is LGPL v3. This is backported to get the modules keyword which
+;; provided in bug#54911.
+(define* (backport/coverage-data->lcov data port #:key (modules #f))
+  "Traverse code coverage information DATA, as obtained with
+`with-code-coverage', and write coverage information in the LCOV format to PORT.
+The report will include all the modules loaded at the time coverage data was
+gathered, even if their code was not executed."
+
+  ;; Output per-file coverage data.
+  (format port "TN:~%")
+  (define source-files
+    (filter
+     (lambda (file)
+       (or (not modules) (member file modules)))
+     (instrumented-source-files data)))
+
+  (for-each (lambda (file)
+              (let ((path (search-path %load-path file)))
+                (if (string? path)
+                    (begin
+                      (format port "SF:~A~%" path)
+                      #;
+                      (for-each dump-function procs)
+                      (for-each (lambda (line+count)
+                                  (let ((line  (car line+count))
+                                        (count (cdr line+count)))
+                                    (format port "DA:~A,~A~%"
+                                            (+ 1 line) count)))
+                                (line-execution-counts data file))
+                      (let-values (((instr exec)
+                                    (instrumented/executed-lines data file)))
+                        (format port "LH: ~A~%" exec)
+                        (format port "LF: ~A~%" instr))
+                      (format port "end_of_record~%"))
+                    (begin
+                      (format (current-error-port)
+                              "skipping source file: ~a~%"
+                              file)))))
+            source-files))
+
 
 (define (get-scm-files dir)
   "Recursively finds all the .scm files in a given directory"
@@ -43,13 +86,19 @@
 
 (define (run-coverage)
   "Run coverage check and write the result out into the lcov.info file"
+  (define goblins-modules
+    (get-scm-files "goblins"))
+
   (define-values (data result)
     (with-code-coverage
      (lambda _
        (test-with-runner
 	   (test-runner-simple)
-	 (map load-from-path (get-scm-files "tests"))))))
+	 (map load-from-path (get-scm-files "tests")))
+       ;; This loads all goblins modules to get the test coverage
+       ;; statistics on each, without it just shows tested modules.
+       (map load-from-path goblins-modules))))
 
   (let ((port (open-output-file "lcov.info")))
-    (coverage-data->lcov data port)))
+    (backport/coverage-data->lcov data port #:modules goblins-modules)))
 (run-coverage)
