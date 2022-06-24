55b63ff96fd044b00a9ba70d7abbf30f7199c663
Christine Lemmer-Webber <cwebber@dustycloud.org>
2022-06-24 14:16:28 -0400

    Starting hacking on Goblins v0.9-pre

--- guix.scm
+++ guix.scm
@@ -30,7 +30,7 @@
 
 (package
   (name "guile-goblins")
-  (version "0.8")
+  (version "0.9-pre")
   (source (local-file (dirname (current-filename))
                       #:recursive? #t
                       #:select? keep-file?))
