70f0a489769af40b1cb15c848a70c6685513b19d
Christine Lemmer-Webber <cwebber@dustycloud.org>
2022-12-08 16:41:05 -0500

    Update try-base-netlayer to use testuds code

--- examples/try-base-netlayer.scm
+++ examples/try-base-netlayer.scm
@@ -1,43 +1,15 @@
 (use-modules (goblins)
-             (goblins ocapn netlayer base-port)
-             (goblins ocapn netlayer utils)
              (goblins ocapn captp)
              (goblins ocapn ids)
-             (goblins utils random-name)
+             (goblins ocapn netlayer testuds)
              (ice-9 getopt-long)
              (ice-9 match)
              (ice-9 curried-definitions))
 
-(define* (^testuds-netlayer bcom netlayers-dir
-                            #:key (machine-id (random-name 32)))
-  (define our-location
-    (make-ocapn-machine 'testuds machine-id '()))
-  (define (sock-filename-for-id id)
-    (string-append netlayers-dir file-name-separator-string id
-                   ".sock"))
-  (define our-sock-filename
-    (sock-filename-for-id machine-id))
-  (define our-server-sock
-    (make-server-unix-domain-socket (sock-filename-for-id machine-id)))
-  (define (incoming-accept)
-    (match (accept our-server-sock SOCK_NONBLOCK)
-      ((client . addr)
-       (setvbuf client 'block 1024)
-       ;; (As said in the Fibers manual:)
-       ;; Disable Nagle's algorithm.  We buffer ourselves.
-       ;; (setsockopt client IPPROTO_TCP TCP_NODELAY 1)
-       client)))
-  (define (outgoing-connect-location location)
-    (unless (eq? (ocapn-machine-transport location) 'testuds)
-      (error "Wrong netlayer! Expected testuds" location))
-    (make-client-unix-domain-socket (sock-filename-for-id
-                                     (ocapn-machine-address location))))
-  (^base-port-netlayer bcom our-location
-                       incoming-accept outgoing-connect-location))
-
 (define netlayers-dir "/tmp/netlayers")
 (unless (file-exists? netlayers-dir)
   (mkdir netlayers-dir))
+
 
 ;;; Evaluate the above, then copy-paste the below to the REPL
 (begin
