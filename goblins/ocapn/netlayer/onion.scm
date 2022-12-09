4061428c32bdbf187e9c48739faaf0e60221f7a4
Christine Lemmer-Webber <cwebber@dustycloud.org>
2022-12-09 12:29:52 -0500

    Disable setsockopt since this is a unix domain socket

--- goblins/ocapn/netlayer/onion.scm
+++ goblins/ocapn/netlayer/onion.scm
@@ -144,7 +144,7 @@
        (setvbuf client 'block 1024)
        ;; (As said in the Fibers manual:)
        ;; Disable Nagle's algorithm.  We buffer ourselves.
-       (setsockopt client IPPROTO_TCP TCP_NODELAY 1)
+       ;; (setsockopt client IPPROTO_TCP TCP_NODELAY 1)
        client)))
   (define (outgoing-connect-location location)
     (unless (eq? (ocapn-machine-transport location) 'onion)
