e379db73bb17b541dfb22ce2a7e94f9b4b88bf3c
Jessica Tallon <tsyesika@tsyesika.se>
2023-01-27 15:26:12 +0100

    Port CapTP tests from racket goblins

--- tests/ocapn/test-captp.scm
+++ tests/ocapn/test-captp.scm
@@ -1,14 +1,127 @@
 (use-modules (goblins)
              (goblins vat)
              (goblins actor-lib joiners)
+             (goblins actor-lib methods)
              (goblins ocapn ids)
              (goblins ocapn captp)
              (goblins ocapn netlayer fake)
              (tests utils)
              (fibers channels)
+             (ice-9 match)
              (srfi srfi-64))
 
 (test-begin "test-captp")
+
+(define (make-new-machine name)
+  "Create a new vat, spawns a fake netlayer & mycapn for given `name'"
+  (define machine-vat (spawn-vat #:name name))
+  (define new-conn-ch (make-channel))
+  (with-vat test-vat
+    ($ test-network 'register name new-conn-ch))
+  (define location (make-ocapn-machine 'fake name #f))
+  (define netlayer
+    (with-vat machine-vat
+     (spawn ^fake-netlayer name test-network new-conn-ch)))
+  (define mycapn
+    (with-vat machine-vat
+     (spawn-mycapn netlayer)))
+  (values machine-vat netlayer mycapn))
+
+(define test-vat (spawn-vat #:name "test"))
+(define test-network
+  (with-vat test-vat
+   (spawn ^fake-network)))
+
+
+;; Spawn different machines.
+(define-values (a-vat a-netlayer a-mycapn)
+  (make-new-machine "a"))
+(define-values (b-vat b-netlayer b-mycapn)
+  (make-new-machine "b"))
+(define-values (c-vat c-netlayer c-mycapn)
+  (make-new-machine "c"))
+
+(define (^greeter _bcom our-name)
+  (lambda (their-name)
+    (format #f "Hello ~a, my name is ~a" their-name our-name)))
+
+(define bob-sref
+  (with-vat b-vat
+    (define bob (spawn ^greeter "Bob"))
+    ($ b-mycapn 'register bob 'fake)))
+
+(let ((result
+       (resolve-vow-and-return-result
+        a-vat
+        (lambda ()
+          (on (<- a-mycapn 'enliven bob-sref)
+              (lambda (bob)
+                (<- bob "Alyssa"))
+              #:promise? #t)))))
+  (test-equal "Non-pipelined send to bob over CapTP"
+    result
+    #(ok "Hello Alyssa, my name is Bob")))
+
+
+;; Testing promise error propagation over CapTP.
+(define (^broken-greeter _bcom our-name)
+  (lambda (their-name)
+    (error 'oh-no-i-broke "My name:" our-name)
+    (format #f "Hello ~a, my name is ~a" their-name our-name)))
+
+(define broken-bob-sref
+  (with-vat b-vat
+    (define broken-bob (spawn ^broken-greeter "Broken Bob"))
+    ($ b-mycapn 'register broken-bob 'fake)))
+
+(let ((result
+       (resolve-vow-and-return-result
+        a-vat
+        (lambda ()
+          (<- (<- a-mycapn 'enliven broken-bob-sref) "Alyssa")))))
+  (test-assert "Promise pipelined breakage propagates across CapTP"
+    (match result
+      [#('err problem) #t]
+      [_ #f])))
+
+;; Testing multiple objects on A, communicating with B
+(define a1-vat (spawn-vat #:name "a1"))
+(define introducer-alice
+  (with-vat a-vat
+    (define (^introducer-alice _bcom)
+      (lambda (intro-bob intro-carol)
+        (<- intro-bob 'meet intro-carol)))
+    (spawn ^introducer-alice)))
+
+(define meeter-bob-sref
+  (with-vat b-vat
+    (define (^meeter-bob _bcom)
+      (methods
+       [(meet new-friend)
+        (<- new-friend 'hi-new-friend)]))
+    (define meeter-bob (spawn ^meeter-bob))
+    ($ b-mycapn 'register meeter-bob 'fake)))
+
+(define (^chatty _bcom our-name)
+  (methods
+   [(hi-new-friend)
+    (list 'hello-back-from our-name)]))
+
+(define chatty-carol
+  (with-vat a1-vat
+    (spawn ^chatty 'carol)))
+
+(let ((result
+       (resolve-vow-and-return-result
+        a-vat
+        (lambda ()
+          (on (<- a-mycapn 'enliven meeter-bob-sref)
+              (lambda (meeter-bob)
+                (<- introducer-alice meeter-bob chatty-carol))
+              #:promise? #t)))))
+  (test-equal "A and C on one machine, B on another with introductions"
+    result
+    #(ok (hello-back-from carol))))
 
 ;; ------------- ;;
 ;; Handoff test  ;;
@@ -25,28 +138,7 @@
   (lambda (greeting)
     greeting))
 
-(define test-vat (spawn-vat #:name "test"))
-(define test-network
-  (with-vat test-vat
-   (spawn ^fake-network)))
-
-(define (make-new-machine name)
-  (define machine-vat (spawn-vat #:name name))
-  (define new-conn-ch (make-channel))
-  (with-vat test-vat
-    ($ test-network 'register name new-conn-ch))
-  (define location (make-ocapn-machine 'fake name #f))
-  (define netlayer
-    (with-vat machine-vat
-     (spawn ^fake-netlayer name test-network new-conn-ch)))
-  (define mycapn
-    (with-vat machine-vat
-     (spawn-mycapn netlayer)))
-  (values machine-vat netlayer mycapn))
-
 ;; Spawn bob on b
-(define-values (b-vat b-netlayer b-mycapn)
-  (make-new-machine "b"))
 (define bob
   (with-vat b-vat
     (spawn ^bob)))
@@ -55,8 +147,6 @@
    ($ b-mycapn 'register bob 'fake)))
 
 ;; Spawn carol on c
-(define-values (c-vat c-netlayer c-mycapn)
-  (make-new-machine "c"))
 (define carol
   (with-vat c-vat
    (spawn ^carol)))
@@ -65,8 +155,6 @@
    ($ c-mycapn 'register carol 'fake)))
 
 ;; Spawn alice on a with a reference to carol and bob
-(define-values (a-vat a-netlayer a-mycapn)
-  (make-new-machine "a"))
 (define alice
   (with-vat a-vat
     ;; Bootstrap bob and carol with sturdyrefs
@@ -84,7 +172,7 @@
         (lambda ()
           (<- alice)))))
   (test-equal
-      "Check Alice on A can handoff Carol on C to Bob on B over CapTP"
+      "Alice on A, Bob on B introduces Carol on C, via handoffs"
     result
     #(ok "Hi Carol, I'm Bob")))
 
@@ -112,5 +200,53 @@
   (test-equal "Sending keyword arguments over CapTP"
     result
     #(ok "a red fork explorist goes vrooom!")))
+
+;; Test promise shortening
+(define sword
+  (with-vat a-vat
+    (spawn (lambda (_bcom) (lambda () 'sword)))))
+(define sword-sref
+  (with-vat a-vat
+    ($ a-mycapn 'register sword 'fake)))
+(define intermediate-land-sref
+  (with-vat b-vat
+    (define (^intermediate-land _bcom)
+      (lambda (farthest-land)
+        (<- farthest-land)))
+    (define intermediate-land (spawn ^intermediate-land))
+    ($ b-mycapn 'register intermediate-land 'fake)))
+(define farthest-land-sref
+  (with-vat c-vat
+    (define (^farthest-land _bcom lost-sword)
+      (lambda ()
+        lost-sword))
+    (define farthest-land
+      (spawn ^farthest-land (<- c-mycapn 'enliven sword-sref)))
+    ($ c-mycapn 'register farthest-land 'fake)))
+(define adventurer
+  (with-vat a-vat
+    (define (^adventurer _bcom sword)
+      (lambda (intermediate-land farthest-land)
+        (on (<- intermediate-land farthest-land)
+            (lambda (returned-sword)
+              `(is-the-same? ,(eq? returned-sword sword)))
+            #:catch
+            (lambda (err)
+              `(error ,err))
+            #:promise? #t)))
+    (spawn ^adventurer sword)))
+
+(let ((result
+       (resolve-vow-and-return-result
+        a-vat
+        (lambda ()
+          (<- adventurer
+              (<- a-mycapn 'enliven intermediate-land-sref)
+              (<- a-mycapn 'enliven farthest-land-sref))))))
+  (test-equal "Shortening promises across CapTP"
+    result
+    #(ok (is-the-same? #t))))
+
+
 
 (test-end "test-captp")
