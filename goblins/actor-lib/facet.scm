0344ecd5107f4b8e77dccac77cc136a251420a05
Juliana Sims <jtsims@protonmail.com>
2023-03-27 11:25:46 -0400

    Add facet.scm docstrings

--- goblins/actor-lib/facet.scm
+++ goblins/actor-lib/facet.scm
@@ -20,6 +20,13 @@
 (define* (^facet bcom wrap-me
                  #:key [sync? #f]
                  #:rest methods)
+  "Construct an object which limits user access to methods of WRAP-ME.
+
+The optional keyword argument SYNC? indicates whether to use $ or <-
+for message proxying. The METHODS argument is the collection of
+methods of WRAP-ME to be exposed to the user.
+
+The resulting actor can be invoke with any of METHODS."
   (define $/<- (if sync? $ <-))
   (lambda args
     (match args
@@ -32,5 +39,12 @@
 (define* (facet wrap-me
                 #:key [sync? #f]
                 #:rest methods)
+  "Return an object which limits user access to methods of WRAP-ME.
+
+The optional keyword argument SYNC? indicates whether to use $ or <-
+for message proxying. The METHODS argument is the collection of
+methods of WRAP-ME to be exposed to the user.
+
+Type: Actor (Optional (#:async? Boolean)) (Symbol ...) -> Actor"
   (apply spawn-named (procedure-name wrap-me) ^facet
          #:sync? sync? methods))
