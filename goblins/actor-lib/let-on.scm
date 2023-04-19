6e0abf05a788a37570f09ef4f994439d3475b358
Juliana Sims <juli@incana.org>
2023-04-19 11:04:01 -0400

    Update copyright attributions

--- goblins/actor-lib/let-on.scm
+++ goblins/actor-lib/let-on.scm
@@ -1,5 +1,6 @@
 ;;; Copyright 2020-2021 Christine Lemmer-Webber
 ;;; Copyright 2023 David Thompson
+;;; Copyright 2023 Juliana Sims
 ;;;
 ;;; Licensed under the Apache License, Version 2.0 (the "License");
 ;;; you may not use this file except in compliance with the License.
