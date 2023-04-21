5512ded7bd1f4b68f750b0f083283807b6ee5d4b
Juliana Sims <juli@incana.org>
2023-04-21 11:17:35 -0400

    Add copyright notices

--- goblins/core.scm
+++ goblins/core.scm
@@ -1,6 +1,7 @@
 ;;; Copyright 2019-2023 Christine Lemmer-Webber
 ;;; Copyright 2023 David Thompson
 ;;; Copyright 2022 Jessica Tallon
+;;; Copyright 2023 Juliana Sims
 ;;;
 ;;; Licensed under the Apache License, Version 2.0 (the "License");
 ;;; you may not use this file except in compliance with the License.
--- goblins/vat.scm
+++ goblins/vat.scm
@@ -1,6 +1,7 @@
 ;;; Copyright 2021-2022 Christine Lemmer-Webber
 ;;; Copyright 2022 Jessica Tallon
 ;;; Copyright 2023 David Thompson
+;;; Copyright 2023 Juliana Sims
 ;;;
 ;;; Licensed under the Apache License, Version 2.0 (the "License");
 ;;; you may not use this file except in compliance with the License.
