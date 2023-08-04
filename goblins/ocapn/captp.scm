513468bfa08bf192a73e19c6d5b37fe24b4884f8
Christine Lemmer-Webber <cwebber@dustycloud.org>
2023-08-04 14:56:30 -0400

    Run setup command on netlayer after it's installed
    This step was probably omitted because we hadn't really had a reason
    to use the install-netlayer command since netlayers moved into a
    multi-step setup process when their design was being finalized.
    Anyway, all the other netlayers get set up one by one when the mycapn
    is being set up, so this is necessary for this one as well... without
    it, the netlayer doesn't know how to speak to the
    connection-establisher.

--- goblins/ocapn/captp.scm
+++ goblins/ocapn/captp.scm
@@ -1608,7 +1608,8 @@
       (define netlayer-name ($C netlayer 'netlayer-name))
       (when ($C netlayer-map 'has-key? netlayer-name)
         (error (format #f "Already has netlayer key ~a" netlayer-name)))
-      ($C netlayer-map 'set netlayer-name netlayer)]
+      ($C netlayer-map 'set netlayer-name netlayer)
+      ($C netlayer 'setup (spawn ^connection-establisher netlayer netlayer-name))]
      [register register]
      [enliven enliven]
      ;; Get the nonce registry used for sturdyrefs to be able to tweak
