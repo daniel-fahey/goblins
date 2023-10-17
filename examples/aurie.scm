488f86f71b00133fb81cff41deb56ab5cfdd8c19
Jessica Tallon <tsyesika@tsyesika.se>
2023-10-17 15:03:22 +0000

    Add some aurie example code

---
+++ examples/aurie.scm
@@ -0,0 +1,50 @@
+(use-modules (goblins)
+             (goblins vat)
+             (goblins contrib aurie)
+             (goblins ghash)
+             (goblins actor-lib methods)
+             (srfi srfi-9)
+             (srfi srfi-11))
+
+;; Test code
+(define* (^robot bcom name #:key [hp 0])
+  (define main-beh
+    (methods
+     [(get-hp) hp]
+     [(get-name) name]
+     [(alive?) (> hp 0)]
+     [(attack damage)
+      (bcom (^robot bcom name #:hp (- hp damage)))]))
+  (define (self-portrait session)
+    (session (list name #:hp hp)))
+
+  (portraitize main-beh self-portrait))
+
+(define (robot-depict name hp)
+  (spawn ^robot name #:hp hp))
+
+(define robot-aurenv
+  (make-aurenv
+    (list (list '((sandbox aurie) ^robot) robot-depict))
+    (list)))
+
+(define my-actormap
+  (make-actormap #:aurenv robot-aurenv))
+
+(define robot1
+  (actormap-spawn! my-actormap ^robot "MegaCrusher3000" #:hp 40))
+
+(define robot2
+  (actormap-spawn! my-actormap ^robot "ElectroSlicer8451" #:hp 50))
+
+(pk 'robot1 robot1 'robot2 robot2)
+
+(define-values (live-portrait portrait-unsealer)
+  (actormap-live-portrait my-actormap))
+
+(pk 'live-portrait live-portrait)
+
+(hash-for-each
+  (lambda (refr portrait)
+    (pk 'refr refr 'portrait (portrait-unsealer portrait)))
+  live-portrait)
