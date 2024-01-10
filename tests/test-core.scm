51b40ece95d7dceaf504599e5df40449ae485780
Jessica Tallon <tsyesika@tsyesika.se>
2024-01-10 15:15:00 +0100

    Add some more tests for aurie

--- tests/test-core.scm
+++ tests/test-core.scm
@@ -628,21 +628,17 @@
  '((got-err yikes) #t))
 
 ;; Aurie tests
-(define* (^aurie-cell bcom #:optional value)
-  (define main-beh
-    (case-lambda
-      [() value]
-      [(new-value)
-       (bcom (^aurie-cell bcom new-value))]))
+(define* (^aurie-incrementer bcom #:optional [value 0])
+  (define (main-beh)
+    (bcom (^aurie-incrementer bcom (+ 1 value)) (+ 1 value)))
   (define (self-portrait)
     (list value))
   (portraitize main-beh self-portrait))
 
 (define* (^aurie-greeter bcom our-name #:optional [init-number-of-times #f])
   (define number-of-times
-    (or init-number-of-times (spawn ^aurie-cell 0)))
+    (or init-number-of-times (spawn ^aurie-incrementer)))
   (define (main-beh your-name)
-    ($ number-of-times (+ ($ number-of-times) 1))
     (bcom (^aurie-greeter bcom our-name number-of-times)
           (format #f "Hello ~a, my name is ~a (called ~a)."
                   your-name our-name ($ number-of-times))))
@@ -668,15 +664,15 @@
      ($ astrid "Johan")
      astrid)))
 
-(define cell-aurenv
+(define incrementer-aurenv
   (make-aurenv
-   (list (make-auriable '((tests test-core) ^aurie-cell) ^aurie-cell))
+   (list (make-auriable '((tests test-core) ^aurie-incrementer) ^aurie-incrementer))
    (list)))
 
 (define greeter-aurenv
   (make-aurenv
    (list (make-auriable '((tests test-core) ^aurie-greeter) ^aurie-greeter restored-greeter-rehydrate))
-   (list cell-aurenv)))
+   (list incrementer-aurenv)))
 
 (define-values (astrid-greeter-depiction _val->slots _slots->val astrid-greeter-roots)
   (actormap-take-portrait first-actormap greeter-aurenv astrid-greeter))
@@ -690,5 +686,28 @@
 (test-equal "Depicted greeter reports correct number of times called after resturation"
   (actormap-peek second-actormap restored-astrid-greeter "Ludvig")
   "Hello Ludvig, my name is *restored Astrid* (called 3).")
+
+(define* (^fancy-greeter bcom our-name #:optional init-number-of-times)
+  (define number-of-times
+    (or init-number-of-times (spawn ^aurie-incrementer)))
+  (define (main-beh your-name)
+    (format #f "Salutations ~a, I am called ~a, delighted to make your acquaintance! (called: ~a)"
+            your-name our-name ($ number-of-times)))
+  (define (self-portrait)
+    (list our-name number-of-times))
+  (portraitize main-beh self-portrait))
+
+(define new-greeter-aurenv
+  (make-aurenv
+   (list (make-auriable '((tests test-core) ^aurie-greeter) ^fancy-greeter))
+   (list incrementer-aurenv)))
+
+(define fancy-astrid-actormap
+  (actormap-replace-behavior second-actormap greeter-aurenv new-greeter-aurenv))
+
+(test-equal "Actors are updated when the behavior is replaced by new behavior"
+  (actormap-peek fancy-astrid-actormap astrid-greeter "Ludvig")
+  "Hello Ludvig, my name is *restored Astrid* (called 3).")
+
 
 (test-end "test-goblins-core")
