e3366ce7bb6f88d8868a42ebd7cd8eee0cc799f5
Jessica Tallon <jessica@spritely.institute>
2024-02-16 15:51:58 +0100

    Add tests for define-actor

--- tests/test-core.scm
+++ tests/test-core.scm
@@ -1,5 +1,6 @@
 ;;; Copyright 2019-2023 Christine Lemmer-Webber
 ;;; Copyright 2023 David Thompson
+;;; Copyright 2024 Jessica Tallon
 ;;;
 ;;; Licensed under the Apache License, Version 2.0 (the "License");
 ;;; you may not use this file except in compliance with the License.
---
+++ tests/utils/test-define-actor.scm
@@ -0,0 +1,57 @@
+;;; Copyright 2024 Jessica Tallon
+;;;
+;;; Licensed under the Apache License, Version 2.0 (the "License");
+;;; you may not use this file except in compliance with the License.
+;;; You may obtain a copy of the License at
+;;;
+;;;    http://www.apache.org/licenses/LICENSE-2.0
+;;;
+;;; Unless required by applicable law or agreed to in writing, software
+;;; distributed under the License is distributed on an "AS IS" BASIS,
+;;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;; See the License for the specific language governing permissions and
+;;; limitations under the License.
+;;;
+(define-module (tests utils test-define-actor)
+  #:use-module (goblins)
+  #:use-module (goblins utils define-actor)
+  #:use-module (fibers)
+  #:use-module (fibers channels)
+  #:use-module (fibers operations)
+  #:use-module (fibers timers)
+  #:use-module (srfi srfi-64))
+
+(test-begin "test-define-actor")
+
+(define-actor (^cell bcom value)
+  (case-lambda
+    [() value]
+    [(new-value) (bcom (^cell bcom new-value))]))
+
+(define am
+  (make-actormap))
+
+(define sward-cell
+  (actormap-run!
+   am
+   (lambda ()
+     (spawn ^cell 'sward))))
+
+(define cell-env
+  (make-persistence-env
+   (list (list '((tests utils test-define-actor) ^cell) ^cell))))
+
+(define-values (portraits roots)
+  (actormap-take-portrait am cell-env sward-cell))
+
+(define restored-am
+  (make-actormap))
+
+(define restored-sward-cell
+  (actormap-restore restored-am cell-env portraits roots))
+
+(test-equal "Got back the sward we put in from the sward cell"
+  (actormap-peek restored-am restored-sward-cell)
+  'sward)
+
+(test-end "test-define-actor")
