e1d239f30585ef1e662275ce2d634e6b55e4911d
Christine Lemmer-Webber <cwebber@dustycloud.org>
2024-02-26 10:14:35 -0500

    Give names to define-actor and add define-hackable

--- goblins/utils/define-actor.scm
+++ goblins/utils/define-actor.scm
@@ -17,27 +17,49 @@
                 #:select (portraitize
                           make-redefinable-object
                           set!-redefinable-object-constructor))
-  #:export (define-actor))
+  #:export (define-actor define-hackable))
 
 (define-syntax-rule (define-actor (constructor-id bcom args ...) body ...)
   (if (module-defined? (current-module) 'constructor-id)
       ;; We've already defined this, just update the constructor refr
       (set!-redefinable-object-constructor
        constructor-id
-       (lambda* (bcom args ...)
-         (define main-beh
-           body ...)
-         (define (self-portrait)
-           (list args ...))
-         (portraitize main-beh self-portrait)))
+       (let ((constructor-id
+              (lambda* (bcom args ...)
+                (define main-beh
+                  body ...)
+                (define (self-portrait)
+                  (list args ...))
+                (portraitize main-beh self-portrait))))
+         constructor-id))
       ;; First time, lets define it.
       (module-define!
        (current-module)
        'constructor-id
        (make-redefinable-object
-        (lambda* (bcom args ...)
-          (define main-beh
-            body ...)
-          (define (self-portrait)
-            (list args ...))
-          (portraitize main-beh self-portrait))))))
+        (let ((constructor-id
+               (lambda* (bcom args ...)
+                 (define main-beh
+                   body ...)
+                 (define (self-portrait)
+                   (list args ...))
+                 (portraitize main-beh self-portrait))))
+          constructor-id)))))
+
+(define-syntax-rule (define-hackable (constructor-id bcom args ...) body ...)
+  (if (module-defined? (current-module) 'constructor-id)
+      ;; We've already defined this, just update the constructor refr
+      (set!-redefinable-object-constructor
+       constructor-id
+       (let ((constructor-id (lambda* (bcom args ...)
+                               body ...)))
+         constructor-id))
+      ;; First time, lets define it.
+      (module-define!
+       (current-module)
+       'constructor-id
+       (make-redefinable-object
+        (let ((constructor-id
+               (lambda* (bcom args ...)
+                 body ...)))
+          constructor-id)))))
