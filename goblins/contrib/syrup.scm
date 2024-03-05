8dcd03bd61aa363f5041f94aafb064f6da29f5ed
Jessica Tallon <tsyesika@tsyesika.se>
2024-03-05 10:54:05 +0100

    Remove the old set implementation from syrup

--- goblins/contrib/syrup.scm
+++ goblins/contrib/syrup.scm
@@ -29,11 +29,7 @@
 
             ;; pseudosingles (pretend to be a single precision float)
             make-pseudosingle pseudosingle?
-            psuedosingle->float
-
-            ;; sets
-            make-set set?
-            set-add set-remove set-fold set->list set-member?))
+            psuedosingle->float))
 
 ;;; Data format
 ;;; ===========
@@ -94,62 +90,6 @@
 
 (define (pseudosingle->float psing)
   (pseudosingle-float psing))
-
-;;; An extremely meh implementation of sets
-
-;;; TODO: make and replace with "gsets"
-
-(define-record-type <set>
-  (_make-set ht)
-  set?
-  (ht _set-ht))
-
-(define (print-set set port)
-  (define items
-    (vhash-fold
-     (lambda (k _v prev)
-       (cons k prev))
-     '()
-     (_set-ht set)))
-  (format port "#<set ~a>" items))
-
-(set-record-type-printer! <set> print-set)
-
-
-(define (make-set . items)
-  (define vh
-    (fold
-     (lambda (item vh)
-       (vhash-consq item #t vh))
-     vlist-null items))
-  (_make-set vh))
-
-(define (set-add set item)
-  (_make-set (vhash-cons item #t (_set-ht set))))
-
-(define (set-remove set item)
-  (_make-set (vhash-delete (_set-ht set) item)))
-
-(define (set-fold proc init set)
-  (vhash-fold
-   (lambda (key _val prev)
-     (proc key prev))
-   init
-   (_set-ht set)))
-
-(define (set->list set)
-  (vhash-fold
-   (lambda (key _val prev)
-     (cons key prev))
-   '()
-   (_set-ht set)))
-
-(define (set-member? set key)
-  (match (vhash-assoc key (_set-ht set))
-    [(_val . #t)
-     #t]
-    [#f #f]))
-
 
 ;;; bytevector utils
 ;;; ================
