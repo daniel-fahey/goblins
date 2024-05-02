e1d55c6bcbd9911f0e9f288d46a682fa4cdc9adb
Jessica Tallon <tsyesika@tsyesika.se>
2024-05-02 14:20:39 +0200

    Decode the libp2p multiaddr into an ocapn-node properly

--- goblins/ocapn/netlayer/libp2p.scm
+++ goblins/ocapn/netlayer/libp2p.scm
@@ -28,7 +28,35 @@
   #:use-module (goblins ocapn netlayer utils)
   #:use-module (goblins ocapn netlayer base-port)
   #:use-module (goblins contrib syrup)
-  #:export (new-libp2p-netlayer))
+  #:export (spawn-libp2p-netlayer
+            ocapn-node->libp2p-multiaddr
+            libp2p-multiaddress->ocapn-node))
+
+(define (libp2p-multiaddress->ocapn-node multiaddr)
+  (unless (string? multiaddr)
+    (error "Non-string libp2p multiaddr provided" multiaddr))
+  (let ((peer-id-seperator (string-rindex multiaddr #\/)))
+    (if (not (integer? peer-id-seperator))
+        (error "Not a valid multiaddr" multiaddr)
+        (make-ocapn-node
+         'libp2p
+         ;; Peer ID is the designator as it uniquely identifies the
+         ;; node
+         (substring multiaddr (+ 1 peer-id-seperator))
+
+         ;; The routing is kept as a string as it's order is important.
+         `((addrs ,(substring multiaddr 0 peer-id-seperator)))))))
+
+(define (ocapn-node->libp2p-multiaddr node)
+  (unless (and (ocapn-node? node) (eq? (ocapn-node-transport node) 'libp2p))
+    (error "Can only convert libp2p OCapN node to libp2p mutliaddr"
+           node))
+  (define addrs
+    (assq-ref (ocapn-node-hints node) 'addrs))
+  (if (list? addrs)
+      (string-join (list (car addrs) (ocapn-node-designator node))
+                   "/")
+      (error "No libp2p multiaddr in OCapN node hints" node)))
 
 (define (build-path . args)
   (string-join args file-name-separator-string))
@@ -39,7 +67,8 @@
 (define default-libp2p-path
   (build-path "/tmp" "goblins-libp2p"))
 
-(define (setup-ocapn-io control-path path)
+(define* (setup-ocapn-io control-path path
+                         #:optional private-key)
   ;; Set up the temporary directory and paths we'll be using for this
   ;; captp process
   (unless (file-exists? path)
@@ -73,43 +102,51 @@
   (define-values (control-in-ch control-out-ch)
     (line-delimited-ports->channels control-sock control-sock))
 
+  (define base-message
+    (format #f "NEW incoming:~a outgoing:~a protocol:ocapn version:1.0.0"
+            incoming-connections-path
+            outgoing-connections-path))
+  
   (put-message control-out-ch
-               (format #f "NEW incoming:~a outgoing:~a protocol:ocapn version:1.0.0"
-                       incoming-connections-path
-                       outgoing-connections-path))
-  (define our-location
-    (match (string-split (get-message control-in-ch) #\space)
-      [("Address:" designator)
-       (make-ocapn-node 'libp2p (designator->encoded-designator designator) #f)]
-      [something (error "Got unknown:" something)]))
+               (if private-key
+                   (format #f "~a private-key:~a" base-message private-key)
+                   base-message))
 
-  (values our-location control-sock
+  (define (split-control-message message)
+    (let ((pairs (string-split message #\space)))
+      (map (lambda (pair)
+             (string-split pair #\:))
+       pairs)))
+  
+  (define-values (our-location new-private-key)
+    (let ((message (get-message control-in-ch)))
+      (match (split-control-message message)
+        [(("address" multiaddr) ("private-key" privkey))
+         (values (libp2p-multiaddress->ocapn-node multiaddr)
+                 privkey)]
+        [something (error "Got unknown:" something)])))
+
+  (values our-location new-private-key
+          control-sock
           incoming-connections-sock
           outgoing-connections-path))
 
-(define (designator->encoded-designator designator)
-  (url-base64-encode
-   (string->bytevector designator "utf-8")))
-
-(define (encoded-designator->designator designator)
-  (bytevector->string
-   (url-base64-decode designator)
-   "utf-8"))
-
-(define (new-libp2p-netlayer)
-  (define-values (our-location control-sock incoming-conn-sock outgoing-conn-path)
+(define* (spawn-libp2p-netlayer #:optional private-key)
+  (define-values (our-location new-private-key control-sock
+                               incoming-conn-sock outgoing-conn-path)
     (setup-ocapn-io default-libp2p-control-path
-                    default-libp2p-path))
-  (spawn ^libp2p-netlayer
-         our-location
-         incoming-conn-sock
-         outgoing-conn-path))
+                    default-libp2p-path
+                    private-key))
+  (values (spawn ^libp2p-netlayer
+                  our-location
+                  incoming-conn-sock
+                  outgoing-conn-path)
+          new-private-key))
 
 (define (setup-outgoing-sock sock location)
   (display
    (format #f "CONNECT ~a\n"
-           (encoded-designator->designator
-            (ocapn-node-designator location)))
+           (ocapn-node->libp2p-multiaddr location))
    sock)
   (flush-output-port sock))
 
