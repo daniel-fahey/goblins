203d710a8af13af2b894a9bac711df8e800af708
David Thompson <dave@spritely.institute>
2024-06-25 11:11:21 -0400

    Add my copyright info for (goblins actor-lib joiners) and tests.

--- tests/actor-lib/test-joiners.scm
+++ tests/actor-lib/test-joiners.scm
@@ -1,4 +1,5 @@
 ;;; Copyright 2022 Jessica Tallon
+;;; Copyright 2024 David Thompson <dave@spritely.institute>
 ;;;
 ;;; Licensed under the Apache License, Version 2.0 (the "License");
 ;;; you may not use this file except in compliance with the License.
