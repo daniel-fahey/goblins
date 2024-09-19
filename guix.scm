5b2a2989a5299fed4ef3eb984cf15b6377198ed0
Jessica Tallon <tsyesika@tsyesika.se>
2024-09-19 17:10:13 +0200

    Update guix.scm package to 0.14.0

--- guix.scm
+++ guix.scm
@@ -31,7 +31,7 @@
 
 (package
   (name "guile-goblins")
-  (version "0.13.0-git")
+  (version "0.14.0-git")
   (source (local-file (dirname (current-filename))
                       #:recursive? #t
                       #:select? keep-file?))
