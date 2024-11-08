f6364f89e9e91f29716647023a29453164148181
Jessica Tallon <tsyesika@tsyesika.se>
2024-11-08 14:41:15 +0100

    Add a hoot compatible base64 module
    Fix #233

--- goblins/utils/crypto.scm
+++ goblins/utils/crypto.scm
@@ -14,15 +14,13 @@
 ;;; limitations under the License.
 
 (define-module (goblins utils crypto)
+  #:use-module (goblins contrib base64)
   #:use-module (gcrypt hash)
   #:use-module ((gcrypt pk-crypto) #:prefix gcrypt:pk-crypto:)
   #:use-module (gcrypt random)
-  #:use-module (gcrypt base64)
   #:use-module (rnrs bytevectors)
   #:export (sha256d
             strong-random-bytes
-            url-base64-encode
-            url-base64-decode
             generate-key-pair
             key-pair->public-key
             key-pair->private-key
@@ -35,21 +33,6 @@
 
 (define (strong-random-bytes byte-size)
   (gen-random-bv 32 %gcry-strong-random))
-
-(define (pad-b64-string b64-str)
-  (define str-len (string-length b64-str))
-  (define mod (modulo str-len 4))
-  (if (zero? mod)
-      b64-str
-      (let ((padme (- 4 mod)))
-        (string-append b64-str
-                       (make-string padme #\=)))))
-
-(define (url-base64-encode bv)
-  (base64-encode bv 0 (bytevector-length bv) #f #t base64url-alphabet))
-
-(define (url-base64-decode encoded-b64)
-  (base64-decode (pad-b64-string encoded-b64) base64url-alphabet))
 
 (define (data->canonical-sexp data)
   (gcrypt:pk-crypto:sexp->canonical-sexp
