c4ec7f9409deb8b3ab0dec868fc1f3fa0ed8704d
Juliana Sims <juli@incana.org>
2024-11-19 15:02:24 -0500

    inbox: Add Guile/Hoot cond-expand.
    * goblins/inbox.scm: Add Guile/Hoot cond-expand.

--- goblins/inbox.scm
+++ goblins/inbox.scm
@@ -59,7 +59,9 @@
   ;; boot it up!
   (syscaller-free
    (lambda ()
-     (spawn-fiber start-inbox-loop scheduler)))
+     (spawn-fiber start-inbox-loop
+                  (cond-expand
+                   (hoot)
+                   (guile scheduler)))))
   ;; return inbox enqueue/dequeue channels, as well as stop operation
   (values enq-ch deq-ch stop?))
-
