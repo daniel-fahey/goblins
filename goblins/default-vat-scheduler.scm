19079eb2dfafc2d02b9966164111dd4d4347adc7
Juliana Sims <juli@incana.org>
2024-11-19 14:57:44 -0500

    default-vat-scheduler: Convert to define-library module.
    * goblins/default-vat-scheduler.scm: Convert to define-library module.

--- goblins/default-vat-scheduler.scm
+++ goblins/default-vat-scheduler.scm
@@ -12,47 +12,47 @@
 ;;; See the License for the specific language governing permissions and
 ;;; limitations under the License.
 
-(define-module (goblins default-vat-scheduler)
-  #:use-module (fibers)
-  #:use-module (fibers channels)
-  #:use-module (fibers conditions)
-  #:use-module ((fibers scheduler)
-                #:select (current-scheduler))
-  #:use-module (ice-9 atomic)
-  #:use-module (ice-9 threads)
-  #:export (default-vat-scheduler))
-
 ;;; The reason for this whole module is to try to prevent creating
 ;;; so many threads that epoll gets mad.  Having a shared scheduler
 ;;; mitigates this.
 
-;; A shared Fibers scheduler to default most vats connecting to.
-;; We might prefer eventually to delay booting this up as long
-;; as possible.
-(define %vat-sched
-  (make-atomic-box #f))
-(define (default-vat-scheduler)
-  "Returns the current default vat scheduler, or makes one if
+(define-library (goblins default-vat-scheduler)
+  (export default-vat-scheduler)
+  (import (guile)
+          (fibers)
+          (fibers channels)
+          (fibers conditions)
+          (only (fibers scheduler)
+                current-scheduler)
+          (ice-9 atomic)
+          (ice-9 threads))
+  (begin
+    ;; A shared Fibers scheduler to default most vats connecting to.
+    ;; We might prefer eventually to delay booting this up as long
+    ;; as possible.
+    (define %vat-sched
+      (make-atomic-box #f))
+    (define (default-vat-scheduler)
+      "Returns the current default vat scheduler, or makes one if
 appropriate"
-  (or (atomic-box-ref %vat-sched)   ; already have a scheduler
-      (begin                        ; or, try to make a new one
-        (let ((result-ch (make-channel)))
-          (make-thread
-           (lambda ()
-             (run-fibers
-              (lambda ()
-                ;; attempt to install this as the current scheduler
-                (define this-sched
-                  (current-scheduler))
-                (define prev-sched
-                  (atomic-box-compare-and-swap! %vat-sched #f this-sched))
-                (if prev-sched
-                    ;; someone else installed a scheduler in-between
-                    (put-message result-ch prev-sched)
-                    ;; our scheduler is the new one
-                    (begin
-                      (put-message result-ch this-sched)
-                      ;; since we're the new scheduler, run forever...
-                      (wait (make-condition))))))))
-          (get-message result-ch)))))
-
+      (or (atomic-box-ref %vat-sched)   ; already have a scheduler
+          (begin                        ; or, try to make a new one
+            (let ((result-ch (make-channel)))
+              (make-thread
+               (lambda ()
+                 (run-fibers
+                  (lambda ()
+                    ;; attempt to install this as the current scheduler
+                    (define this-sched
+                      (current-scheduler))
+                    (define prev-sched
+                      (atomic-box-compare-and-swap! %vat-sched #f this-sched))
+                    (if prev-sched
+                        ;; someone else installed a scheduler in-between
+                        (put-message result-ch prev-sched)
+                        ;; our scheduler is the new one
+                        (begin
+                          (put-message result-ch this-sched)
+                          ;; since we're the new scheduler, run forever...
+                          (wait (make-condition))))))))
+              (get-message result-ch)))))))
