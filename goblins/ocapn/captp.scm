2a42dbb2470eaeaa5fa134a6e74f040a22383dfc
Jessica Tallon <tsyesika@tsyesika.se>
2024-12-05 15:26:40 +0100

    Remove unused import in CapTP

--- goblins/ocapn/captp.scm
+++ goblins/ocapn/captp.scm
@@ -46,7 +46,6 @@
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-11)
   #:use-module (rnrs bytevectors)
-  #:use-module (rnrs io ports)
   #:use-module (fibers channels)
   #:export (spawn-mycapn captp-env))
 
