f0e64f398288b3bbca9c5317bc0f6f9881498c20
Jessica Tallon <tsyesika@tsyesika.se>
2025-02-28 12:23:54 +0100

    Fix indentation removing tabs

--- goblins/core.scm
+++ goblins/core.scm
@@ -1,6 +1,6 @@
 ;;; Copyright 2019-2023 Christine Lemmer-Webber
 ;;; Copyright 2023 David Thompson
-;;; Copyright 2022-2024 Jessica Tallon
+;;; Copyright 2022-2025 Jessica Tallon
 ;;; Copyright 2023 Juliana Sims
 ;;;
 ;;; Licensed under the Apache License, Version 2.0 (the "License");
@@ -3128,10 +3128,10 @@ Type: Actormap PersistenceEnv -> Void"
   ;; When an actor is spawned it might send messages
   ;; so keep track of those so we can dispatch them after.
   (while (not (q-empty? near-msg-queue))
-	 (let-values (((result new-am new-msgs)
-		       (actormap-turn-message am (deq! near-msg-queue))))
-	   (transactormap-merge! new-am)
-	   (enq-msgs! new-msgs)))
+    (let-values (((result new-am new-msgs)
+                  (actormap-turn-message am (deq! near-msg-queue))))
+      (transactormap-merge! new-am)
+      (enq-msgs! new-msgs)))
 
   ;; Dispatch the far messages.
   (while (not (q-empty? far-msg-queue))
