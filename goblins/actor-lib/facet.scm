37c716cdd77907d4cbc7739a29dc7ac56bd8f3bf
Vivianne Langdon <puttabutta@gmail.com>
2025-08-08 23:20:36 -0400

    Use refr-name and ^facet otherwise

--- goblins/actor-lib/facet.scm
+++ goblins/actor-lib/facet.scm
@@ -15,7 +15,6 @@
 
 (define-module (goblins actor-lib facet)
   #:use-module (goblins)
-  #:use-module (goblins core-types)
   #:use-module (goblins actor-lib opportunistic)
   #:use-module (ice-9 match)
   #:export (^facet facet facet-env))
@@ -48,14 +47,7 @@ The METHODS argument is the collection of methods of WRAP-ME to be
 exposed to the user.
 
 Type: Actor (Symbol ...) -> Actor"
-  (define name
-    (match wrap-me
-      ((? local-object-refr?) (local-object-refr-debug-name wrap-me))
-      ((? local-promise-refr?) 'promise)
-      ((? remote-promise-refr?) 'remote-promise)
-      ((? remote-object-refr?) 'remote-object)
-      (else (error "Unexpected object type"))))
-
+  (define name (or (refr-name wrap-me) '^facet))
   (apply spawn-named name ^facet wrap-me methods))
 
 (define facet-env
