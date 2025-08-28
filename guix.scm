4824f16902acd520fa3161419040e20e51ddf1cd
David Thompson <dave@spritely.institute>
2025-08-28 09:16:25 -0400

    Bump version to 0.16.1.

--- guix.scm
+++ guix.scm
@@ -33,7 +33,7 @@
 
 (package
   (name "guile-goblins")
-  (version "0.16.0-git")
+  (version "0.16.1-git")
   (source (local-file (dirname (current-filename))
                       #:recursive? #t
                       #:select? keep-file?))
